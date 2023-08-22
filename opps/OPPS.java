bbf1ed780478e68e4b7bdbab4bf043579f5c562d
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-22 11:52:18 -0700

    updated

--- opps/OPPS.java
+++ opps/OPPS.java
@@ -15,10 +15,13 @@ class Student{
         System.out.println(this.name);
         System.out.println(this.age);
     }
+    Student(){
+        System.out.println("constructor called");
+    }
 }
 public class OPPS{
     public static void main(String[] args) {
-        Student s1= new Student();
+        Student s1= new Student();//new keyword is used to allocate space in memory where object is stored
         s1.name="aditya";
         s1.age=22;
         s1.printInfo();
