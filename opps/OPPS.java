ca095367e844609c5ee0c77946a709cfc93e5eaf
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-22 11:35:11 -0700

    updated

--- opps/OPPS.java
+++ opps/OPPS.java
@@ -8,19 +8,20 @@ class Pen{
         System.out.println(this.color);//
     }
 }
+class Student{
+    String name;
+    int age;
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
 public class OPPS{
     public static void main(String[] args) {
-        Pen pen1=new Pen();
-        pen1.color="blue";
-        pen1.type="gel";
-
-        Pen pen2 = new Pen();
-        pen2.color="black";
-        pen2.type="ballpoint";
-
-        pen1.printColor();
-        pen2.printColor();
-
+        Student s1= new Student();
+        s1.name="aditya";
+        s1.age=22;
+        s1.printInfo();
         
     }
 }
