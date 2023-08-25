4d1a366fe17312d18185e0729480df40ad527dfc
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-25 06:27:13 -0700

    updtaed

---
+++ opps/Array.java
@@ -0,0 +1,13 @@
+public class Array{
+    public static void main(String[] args) {
+        int age =30;
+        int physics= 97;
+        int chem = 98;
+        int eng = 95;
+        int [] marks = new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+        System.out.println(marks[0]);
+    }
+}
