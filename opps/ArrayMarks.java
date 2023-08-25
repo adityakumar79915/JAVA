ee7bfbb8f59628714e8768d0db1c14c0f88a7bba
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-25 06:38:45 -0700

    updated

---
+++ opps/ArrayMarks.java
@@ -0,0 +1,7 @@
+public class ArrayMarks {
+    public static void main(String[] args) {
+        int[]marks={97,98,95};
+        int[][]finalmarks={{97,98,95},{95,95,98}};
+        System.out.println(finalmarks[1][1]);
+    }
+}
