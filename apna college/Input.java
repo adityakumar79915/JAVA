ff1737de8503c8e9c0f26ef8df46e53cbeffc7b3
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-26 11:19:05 -0700

    updated

---
+++ apna college/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        //how to take input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input your age :");
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
