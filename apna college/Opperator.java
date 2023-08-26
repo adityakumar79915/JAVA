fddd1615848f35b53e9c6ab1d89e8bce45c32e27
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-26 11:04:39 -0700

    updated

---
+++ apna college/Opperator.java
@@ -0,0 +1,16 @@
+public class Opperator {
+    public static void main(String[] args) {
+        //operators
+        int a = 1;
+        int b = 2;
+        int sum = a+b;
+        int diff = a-b;
+        int div =a/b;
+        int mul =a*b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(div);
+        System.out.println(mul);
+
+    }
+}
