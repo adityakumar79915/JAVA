0ac01b23e911c9efaae5c29f73bf4bc01e977101
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-27 02:58:46 -0700

    updated

--- apna college/Conditional.java
+++ apna college/Conditional.java
@@ -8,5 +8,12 @@ public class Conditional {
         {
             System.out.println("night");
         }
+        int age =30;
+        if(age>30){
+            System.out.println("can vote");
+        }
+        else{
+            System.out.println("cannot vote");
+        }
     }
 }
