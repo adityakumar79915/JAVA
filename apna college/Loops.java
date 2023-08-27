38fb8f74126e8f6e652d325f7e47487056290b46
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-27 03:44:58 -0700

    updated

--- apna college/Loops.java
+++ apna college/Loops.java
@@ -3,5 +3,10 @@ public class Loops {
         for(int i=100; i>=0; i--){
             System.out.println(i);
         }
+        int j =100;
+        while(j>=1){
+            System.out.println(j);
+            j--;
+        }
     }
 }
