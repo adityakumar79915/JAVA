30521bd1d03ad4640f2e7798663f063772594a4e
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-28 09:23:35 -0700

    updated

--- apna college/Array.java
+++ apna college/Array.java
@@ -13,3 +13,5 @@ public class Array {
         }
     }
 }
+//An array in Java is a data structure that allows you to store multiple values of the same type
+//Zero-based Indexing: Array indices start from 0. The first element is at index 0, the second at index 1, and so on.
