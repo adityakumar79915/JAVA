813de40f4bc78d8d75466b94468a6e6a04bd65b3
adityakumar79915 <adityakumar79915@gmail.com>
2023-08-30 07:10:40 -0700

    updated

---
+++ exam/Typecast.java
@@ -0,0 +1,10 @@
+public class Typecast {
+    public static void main(String[] args) {
+        int x = 7;
+        long y= x;
+        float z = y;
+        System.out.println("before conversion,int value"+x);
+        System.out.println("after conversion,long value"+y);
+        System.out.println("after conversion,float value"+z);
+    }
+}
