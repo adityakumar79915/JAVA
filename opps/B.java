ac3e7bacec1f31aac2431941fc8b7b3eebb64d25
adityakumar79915 <adityakumar79915@gmail.com>
2023-09-26 01:06:32 -0700

    updated

--- opps/B.java
+++ opps/B.java
@@ -1,8 +1,25 @@
-package mypack;
-import pack.*;
-public class B{
-public static void main(String args[]){
-A obj = new A();
-obj.msg();
+class A{
+    public void display()
+    {
+        System.out.println("hello 1");
+    }
 }
+public class B extends A{
+    public void display(){
+        System.out.println("hello 2");
+    }
+    public void greet(){
+        System.out.println("how are you");
+    }
+    public void show(){
+        display();
+        greet();
+        super.display();
+        //super.greet();
+    }
+    public static void main(String[] args) {
+        B b = new B();
+        b.show();
+
+    }
 }
