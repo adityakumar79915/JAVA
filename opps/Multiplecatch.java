d74784082a9303395211ce3cd6087e288027497e
adityakumar79915 <adityakumar79915@gmail.com>
2023-09-27 07:16:08 -0700

    updated

---
+++ opps/Multiplecatch.java
@@ -0,0 +1,18 @@
+public class Multiplecatch {
+    public static void main(String[] args) {
+        try{
+            int a[]= new int[5];
+            a[5]=30/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println("Arithmetic Exception occur");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBounds Exception occurs");
+        }
+        catch(Exception e){
+            System.out.println("Parent Exception Occurs");
+        }
+        System.out.println("end of the code");
+    }
+}
